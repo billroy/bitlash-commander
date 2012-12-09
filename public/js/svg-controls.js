c8de6f2a6581c7dd9ec73c3adc59a152cf715eb2
Bill Roy <bill@bitlash.net>
2012-12-09 06:35:30 -0700

    indicator for repeating buttons.  contextmenu

--- public/js/svg-controls.js
+++ public/js/svg-controls.js
@@ -435,8 +435,19 @@ console.log('Drag start:', x, y, event);
 				this.running = false;
 				clearInterval(this.intervalid);
 				delete this.intervalid;
+				this.runningindicator.remove();
+				delete this.runningindicator;
 			} else {
 				this.running = true;
+				var translation = ['t', this.x, ',', this.y, 's', .75].join('');
+
+				this.runningindicator = this.parent.paper.path('M19.275,3.849l1.695,8.56l1.875-1.642c2.311,3.59,1.72,8.415-1.584,11.317c-2.24,1.96-5.186,2.57-7.875,1.908l-0.84,3.396c3.75,0.931,7.891,0.066,11.02-2.672c4.768-4.173,5.521-11.219,1.94-16.279l2.028-1.775L19.275,3.849zM8.154,20.232c-2.312-3.589-1.721-8.416,1.582-11.317c2.239-1.959,5.186-2.572,7.875-1.909l0.842-3.398c-3.752-0.93-7.893-0.067-11.022,2.672c-4.765,4.174-5.519,11.223-1.939,16.283l-2.026,1.772l8.26,2.812l-1.693-8.559L8.154,20.232z')
+					.transform(translation)
+					.attr({fill:this.stroke, stroke: this.stroke})
+					.click(function(e) { 
+//						self.addButton({x:self.next_x+=self.next_inc, y:self.next_y+=self.next_inc});
+					});
+					
 				this.exec();
 			}
 		}
@@ -471,19 +482,6 @@ console.log('Drag start:', x, y, event);
 			this.intervalid = setInterval(function() { self.exec.call(self, {}); }, this.repeat);
 		}
 	},
-
-/*
-	handleReply: function(reply) {
-		console.log("UNEXPECTED REPLY");
-		if (reply === undefined) return;
-		this.reply = reply.trim();
-		if (this.reply.length == 0) return;
-		this.setValue(this.reply);
-		var update = {id: this.id, value: this.value};
-		this.parent.sendUpdate('update', update);
-		this.fire('update', update);
-	},
-*/
 	
 	setValue: function(value) {
 		this.value = value;
